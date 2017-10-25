100e7c1504ddeb7fb17e9ea5001766d97c03d753
Anton Davydov <dydus0x14@gmail.com>
2017-10-25 23:41:52 +0300

    Init

---
+++ rules/rules/rules.h
@@ -0,0 +1,19 @@
+//
+//  rules.h
+//  rules
+//
+//  Created by Anton Davydov on 25/10/2017.
+//  Copyright © 2017 Anton Davydov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for rules.
+FOUNDATION_EXPORT double rulesVersionNumber;
+
+//! Project version string for rules.
+FOUNDATION_EXPORT const unsigned char rulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rules/PublicHeader.h>
+
+
---
+++ rules/rules/rules.swift
@@ -0,0 +1,9 @@
+//
+//  rules.swift
+//  rules
+//
+//  Created by Anton Davydov on 25/10/2017.
+//  Copyright © 2017 Anton Davydov. All rights reserved.
+//
+
+import Foundation
